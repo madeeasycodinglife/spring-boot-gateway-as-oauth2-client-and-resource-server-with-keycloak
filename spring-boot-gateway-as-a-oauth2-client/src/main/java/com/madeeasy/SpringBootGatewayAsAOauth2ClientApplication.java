2a3d2ce3ad4b54f567495b1f1fd48a1c33b0cc03
Pabitra Bera <126568738+madeeasycodinglife@users.noreply.github.com>
2023-06-14 10:04:41 +0530

    Add files via upload

---
+++ spring-boot-gateway-as-a-oauth2-client/src/main/java/com/madeeasy/SpringBootGatewayAsAOauth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.madeeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGatewayAsAOauth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGatewayAsAOauth2ClientApplication.class, args);
+	}
+
+}
